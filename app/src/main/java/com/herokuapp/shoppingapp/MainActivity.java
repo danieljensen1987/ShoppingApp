1b37a9a20fbfd3fff9aecf680b062553c64245f8
Daniel Jensen <dj@dschool.dk>
2015-04-22 14:53:27 +0200

    Working HTTP Post inside a thread

--- app/src/main/java/com/herokuapp/shoppingapp/MainActivity.java
+++ app/src/main/java/com/herokuapp/shoppingapp/MainActivity.java
@@ -1,17 +1,87 @@
 package com.herokuapp.shoppingapp;
 
+import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+
 
 
 public class MainActivity extends ActionBarActivity {
+    Button btn_login;
+    Button btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Init();
     }
+
+    public void Init(){
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+    }
+
+    public void Login(View v) throws JSONException, IOException {
+        final Thread t = new Thread() {
+
+            public void run() {
+                Looper.prepare();
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
+                HttpResponse response;
+                JSONObject obj = new JSONObject();
+
+
+                try {
+                    HttpPost post = new HttpPost("https://shoppingapi.herokuapp.com/login");
+
+                    obj.put("email", "Frederik.o@mailme.dk");
+                    obj.put("password", "Test12345678");
+
+
+                    StringEntity se = new StringEntity(obj.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    //post.setHeader("host", "http://pc.dyndns-office.com/mobile.asp");
+
+                    response = client.execute(post);
+
+                    if (response != null) {
+                        String responseBody = EntityUtils.toString(response.getEntity());
+                        Toast.makeText(getApplicationContext(), responseBody,Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Looper.loop();
+            }
+        };
+
+        t.start();
+    }
+
+
 
 
     @Override
