3d7e19db6a7f9795641170d40fc32b64bacb57c0
unknown <fol@jetnb255.jet-time.local>
2015-04-22 11:38:15 +0200

    Test

--- app/src/main/java/com/herokuapp/shoppingapp/MainActivity.java
+++ app/src/main/java/com/herokuapp/shoppingapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //Test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
